4ef074ff219c8f929229bf7d9a23579c480799bb
Hleb Bandarenka <bhleb@amazon.com>
2023-03-18 12:45:26 -0700

    simplified 206

--- src/main/java/com/leetcode/linkedlist/easy/ReverseLinkedList_206_Impl.java
+++ src/main/java/com/leetcode/linkedlist/easy/ReverseLinkedList_206_Impl.java
@@ -6,15 +6,12 @@ public class ReverseLinkedList_206_Impl implements ReverseLinkedList_206 {
 
   @Override
   public ListNode reverseList(ListNode head) {
-    if (head == null) {
-      return null;
-    }
     ListNode prev = null;
     while (head != null) {
-      ListNode tmp = head.next;
-      head.next = prev;
-      prev = head;
-      head = tmp;
+      ListNode tmp = head;
+      head = tmp.next;
+      tmp.next = prev;
+      prev = tmp;
     }
 
     return prev;
