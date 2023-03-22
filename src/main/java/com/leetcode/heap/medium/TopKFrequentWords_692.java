b463e81dd49157c39fafecc3f4ba2c61da5273dd
Hleb Bandarenka <bhleb@amazon.com>
2023-03-21 22:56:01 -0700

    added link in 692

--- src/main/java/com/leetcode/heap/medium/TopKFrequentWords_692.java
+++ src/main/java/com/leetcode/heap/medium/TopKFrequentWords_692.java
@@ -2,6 +2,10 @@ package com.leetcode.heap.medium;
 
 import java.util.List;
 
+/**
+ * Ref: <a href="https://leetcode.com/problems/top-k-frequent-words">692. Top K Frequent Words</a>
+ */
 public interface TopKFrequentWords_692 {
+
   List<String> topKFrequent(String[] words, int k);
 }
