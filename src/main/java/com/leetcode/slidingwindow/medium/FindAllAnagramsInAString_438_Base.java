254a446a35188e3f5046d4d2486ba9af02b69f95
Hleb Bandarenka <bhleb@amazon.com>
2023-03-25 12:40:26 -0700

    clarified 438

--- src/main/java/com/leetcode/slidingwindow/medium/FindAllAnagramsInAString_438_Base.java
+++ src/main/java/com/leetcode/slidingwindow/medium/FindAllAnagramsInAString_438_Base.java
@@ -3,51 +3,57 @@ package com.leetcode.slidingwindow.medium;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FindAllAnagramsInAString_438_Base implements
-    FindAllAnagramsInAString_438 {
+public class FindAllAnagramsInAString_438_Base implements FindAllAnagramsInAString_438 {
 
   @Override
   public List<Integer> findAnagrams(final String s, final String p) {
+    // target result
     final List<Integer> result = new ArrayList<>();
 
-    final int sourceLength = s.length();
-    final int patternLength = p.length();
+    // global context - patternFrequency
+    final int[] patternFrequency = generateFrequency(p);
 
-    final int[] patternFrequency = getFrequency(p);
+    // current context - sourceFrequency + left
     final int[] sourceFrequency = new int[26];
+    int left = 0; // TODO you can use p.length() to calculate Left
 
-    for (int i = 0; i < sourceLength; i++) {
+    // loop
+    for (int i = 0; i < s.length(); i++) {
       sourceFrequency[getIndex(s.charAt(i))]++;
-      if (i >= patternLength) {
-        sourceFrequency[getIndex(s.charAt(i - patternLength))]--;
+      int currentLength = i - left + 1;
+
+      // TODO you can calculate initial sourceFrequency before the loop
+      if (currentLength > p.length()) {
+        sourceFrequency[getIndex(s.charAt(left))]--;
+        left++;
       }
+
+      // TODO: you can save previous result to check if you need compare new combination
       if (areSame(sourceFrequency, patternFrequency)) {
-        result.add(i - patternLength + 1);
+        result.add(left);
       }
     }
 
     return result;
   }
 
-  private int[] getFrequency(final String s) {
-    final int[] result = new int[26];
+  private boolean areSame(final int[] source, final int[] target) {
+    for (int i = 0; i < source.length; i++) {
+      if (source[i] != target[i]) return false;
+    }
+    return true;
+  }
+
+
+  private int[] generateFrequency(final String s) {
+    int[] result = new int[26];
     for (final char c : s.toCharArray()) {
       result[getIndex(c)]++;
     }
-
     return result;
   }
 
   private int getIndex(final char c) {
     return c - 'a';
-  }
-
-  private boolean areSame(final int[] source, final int[] target) {
-    for (int i = 0; i < source.length; i++) {
-      if (source[i] != target[i]) {
-        return false;
-      }
-    }
-    return true;
   }
 }
