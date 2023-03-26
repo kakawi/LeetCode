cf6443c607b5ea2e9b5409a58cc60fbdbde42a95
Hleb Bandarenka <bhleb@amazon.com>
2023-03-26 15:34:49 -0700

    implemented StackTraversal

---
+++ src/main/java/com/leetcode/tree/StackTraversal.java
@@ -0,0 +1,98 @@
+package com.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class StackTraversal implements TreeTraversal {
+
+  @Override
+  public List<Integer> inOrder(TreeNode root) {
+    final List<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+
+    final Stack<TreeNode> stack = new Stack<>();
+    while (!stack.isEmpty() || root != null) {
+      while (root != null) {
+        stack.add(root);
+        root = root.left;
+      }
+      root = stack.pop();
+      result.add(root.val);
+      root = root.right;
+    }
+
+    return result;
+  }
+
+  @Override
+  public List<Integer> preOrder(TreeNode root) {
+    final List<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+
+    final Stack<TreeNode> stack = new Stack<>();
+    while (!stack.isEmpty() || root != null) {
+      while (root != null) {
+        result.add(root.val);
+        stack.add(root);
+        root = root.left;
+      }
+      root = stack.pop().right;
+    }
+
+    return result;
+  }
+
+  @Override
+  public List<Integer> postOrder(final TreeNode root) {
+    final List<Integer> result = new ArrayList<>();
+    if (root == null) {
+      return result;
+    }
+
+    final Stack<TreeNode> stack = new Stack<>();
+    stack.push(root);
+    TreeNode prev = null;
+    while (!stack.isEmpty()) {
+      TreeNode current = stack.peek();
+      /* go DOWN the tree in search of a leaf an if so
+      process it and pop stack otherwise move down */
+      if (prev == null || prev.left == current || prev.right == current) {
+        if (current.left != null) {
+          stack.push(current.left);
+        } else if (current.right != null) {
+          stack.push(current.right);
+        } else {
+          stack.pop();
+          result.add(current.val);
+        }
+ 
+      /* go UP the tree from left node, if the
+      child is right push it onto stack otherwise
+      process parent and pop stack */
+      } else if (current.left == prev) {
+        if (current.right != null) {
+          stack.push(current.right);
+        } else {
+          stack.pop();
+          result.add(current.val);
+        }
+ 
+      /* go UP the tree from right node and after
+      coming back from right node process parent
+      and pop stack */
+      } else if (current.right == prev) {
+        stack.pop();
+        result.add(current.val);
+      }
+
+      prev = current;
+    }
+
+    return result;
+  }
+}
--- src/test/java/com/leetcode/tree/TreeTraversalTest.java
+++ src/test/java/com/leetcode/tree/TreeTraversalTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 class TreeTraversalTest {
 
-  private final TreeTraversal solution = new MorrisTraversal();
+  private final TreeTraversal solution = new StackTraversal();
 
   @Test
   void inOrder() {
@@ -37,6 +37,33 @@ class TreeTraversalTest {
 
     // expected
     final List<Integer> expected = List.of(1, 2, 4, 5, 6, 3);
+
+    // when
+    final List<Integer> result = solution.preOrder(root);
+
+    // then
+    assertEquals(expected, result);
+  }
+
+  /**
+   * Ref: https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
+   */
+  @Test
+  void preOrder_2() {
+    // given
+    final TreeNode root = new TreeNode(25,
+        new TreeNode(15,
+            new TreeNode(10, new TreeNode(4), new TreeNode(12)),
+            new TreeNode(22, new TreeNode(18), new TreeNode(24))
+        ),
+        new TreeNode(50,
+            new TreeNode(35, new TreeNode(31), new TreeNode(44)),
+            new TreeNode(70, new TreeNode(66), new TreeNode(90))
+        )
+    );
+
+    // expected
+    final List<Integer> expected = List.of(25, 15, 10, 4, 12, 22, 18, 24, 50, 35, 31, 44, 70, 66, 90);
 
     // when
     final List<Integer> result = solution.preOrder(root);
