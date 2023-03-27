f3f7ba0bfca74480e7d49c3ab8b663772a59aae2
Hleb Bandarenka <bhleb@amazon.com>
2023-03-26 17:51:34 -0700

    improved 121

--- src/main/java/com/leetcode/greedy/easy/BestTimeToBuyAndSellStock_121_Impl.java
+++ src/main/java/com/leetcode/greedy/easy/BestTimeToBuyAndSellStock_121_Impl.java
@@ -1,18 +1,20 @@
 package com.leetcode.greedy.easy;
 
-public class BestTimeToBuyAndSellStock_121_Impl implements BestTimeToBuyAndSellStock_121 {
+public class BestTimeToBuyAndSellStock_121_Impl implements
+    BestTimeToBuyAndSellStock_121 {
 
   @Override
   public int maxProfit(final int[] prices) {
+    int maxProfit = 0;
     int minPrice = prices[0];
-    int profit = 0;
-    for (int i = 1; i < prices.length; i++) {
-      if (minPrice > prices[i]) {
-        minPrice = prices[i];
-      } else if (prices[i] - minPrice > profit) {
-        profit = prices[i] - minPrice;
+    for (final int currentPrice : prices) {
+      if (currentPrice < minPrice) {
+        minPrice = currentPrice;
+        continue;
       }
+      int profit = currentPrice - minPrice;
+      maxProfit = Math.max(maxProfit, profit);
     }
-    return profit;
+    return maxProfit;
   }
 }
