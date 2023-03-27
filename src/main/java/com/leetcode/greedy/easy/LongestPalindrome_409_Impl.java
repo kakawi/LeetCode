7f326d79acac4b2757377257f07d78a4b6e8b393
Hleb Bandarenka <bhleb@amazon.com>
2023-03-26 17:55:51 -0700

    clarified 409

--- src/main/java/com/leetcode/greedy/easy/LongestPalindrome_409_Impl.java
+++ src/main/java/com/leetcode/greedy/easy/LongestPalindrome_409_Impl.java
@@ -4,20 +4,21 @@ public class LongestPalindrome_409_Impl implements LongestPalindrome_409 {
 
   @Override
   public int longestPalindrome(final String s) {
-    int[] numberOfLetters = new int[123];
-    int result = 0;
-    for (final char currentChar : s.toCharArray()) {
-      final int elements = ++numberOfLetters[currentChar - 'A'];
-      if (elements == 2) {
-        result++;
-        numberOfLetters[currentChar - 'A'] = 0;
+    boolean[] seenLetters = new boolean[256];
+    int pairs = 0;
+    for (final char letter : s.toCharArray()) {
+      if (seenLetters[letter]) {
+        pairs++;
+        seenLetters[letter] = false;
+      } else {
+        seenLetters[letter] = true;
       }
     }
 
-    if (s.length() > result * 2) {
-      return result * 2 + 1;
+    if (s.length() > pairs * 2) {
+      return pairs * 2 + 1;
     }
 
-    return result * 2;
+    return pairs * 2;
   }
 }
