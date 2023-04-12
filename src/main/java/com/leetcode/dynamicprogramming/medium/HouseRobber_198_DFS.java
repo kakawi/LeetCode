0ab6d7c96ab0c6fb368a9fd6d9d6efdd65a7451e
Hleb Bandarenka <bhleb@amazon.com>
2023-04-11 22:16:40 -0700

    improved 198

--- src/main/java/com/leetcode/dynamicprogramming/medium/HouseRobber_198_DFS.java
+++ src/main/java/com/leetcode/dynamicprogramming/medium/HouseRobber_198_DFS.java
@@ -1,24 +1,30 @@
 package com.leetcode.dynamicprogramming.medium;
 
+import java.util.Arrays;
+
 public class HouseRobber_198_DFS implements HouseRobber_198 {
+
+  private static final int EMPTY_VALUE = -1;
 
   @Override
   public int rob(int[] nums) {
-    int[] maxProfit = new int[nums.length];
-    for (int i = 0; i < nums.length; i++) {
-      maxProfit[i] = -1;
-    }
-    return solve(nums, 0, maxProfit);
+    final int[] memoization = new int[nums.length];
+    Arrays.fill(memoization, EMPTY_VALUE);
+    return solve(nums, 0, memoization);
   }
 
-  public int solve(int[] nums, int i, int[] protif) {
+  public int solve(int[] nums, int i, int[] memoization) {
     if (i >= nums.length) {
       return 0;
-    } else {
-      if (protif[i] != -1) {
-        return protif[i];
-      }
-      return protif[i] = Math.max(nums[i] + solve(nums, i + 2, protif), solve(nums, i + 1, protif));
     }
+
+    if (memoization[i] != EMPTY_VALUE) {
+      return memoization[i];
+    }
+
+    return memoization[i] = Math.max(
+        nums[i] + solve(nums, i + 2, memoization),
+        solve(nums, i + 1, memoization)
+    );
   }
 }
--- src/main/java/com/leetcode/dynamicprogramming/medium/HouseRobber_198_Impl.java
+++ src/main/java/com/leetcode/dynamicprogramming/medium/HouseRobber_198_Impl.java
@@ -1,39 +1,19 @@
 package com.leetcode.dynamicprogramming.medium;
 
-/**
- * If there are just 5 houses the algorithm is following:
- * Starting from 3rd house we have to choose what is bigger
- * - 1st
- * - 1st OR 2sd
- * - 3rd house + 1st OR (1st or 2nd)
- * - 4th house + 2nd OR 3rd + 1st
- * - 5th house + 3rd + 1st OR 4th + 2nd
- * ...
- */
 public class HouseRobber_198_Impl implements HouseRobber_198 {
 
   @Override
-  public int rob(int[] nums) {
-    if (nums == null || nums.length == 0) {
-      return 0;
-    }
+  public int rob(final int[] nums) {
     if (nums.length == 1) {
       return nums[0];
     }
-    if (nums.length == 2) {
-      return Math.max(nums[0], nums[1]);
+    // 2nd house is a unique case
+    nums[1] = Math.max(nums[0], nums[1]);
+
+    for (int i = 2; i < nums.length; i++) {
+      nums[i] = Math.max(nums[i - 2] + nums[i], nums[i - 1]);
     }
 
-    // maxProfit[i] = the maximum amount of money that can rob to the i-th house
-    int[] maxProfit = new int[nums.length];
-    maxProfit[0] = nums[0];
-    maxProfit[1] = Math.max(nums[0], nums[1]);
-
-    for (int i = 2; i < maxProfit.length; i++) {
-      // maxProfit[i-2] because don't want to trigger the police
-      maxProfit[i] = Math.max(nums[i] + maxProfit[i - 2], maxProfit[i - 1]);
-    }
-
-    return maxProfit[nums.length - 1];
+    return nums[nums.length - 1];
   }
 }
