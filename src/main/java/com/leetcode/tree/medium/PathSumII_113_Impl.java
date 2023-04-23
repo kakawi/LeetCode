626fc3b2a533a2e4021aaa651f5ed0dea5a3ec9e
Hleb Bandarenka <bhleb@amazon.com>
2023-04-23 15:22:38 -0700

    clarified 113

--- src/main/java/com/leetcode/tree/medium/PathSumII_113_Impl.java
+++ src/main/java/com/leetcode/tree/medium/PathSumII_113_Impl.java
@@ -3,38 +3,45 @@ package com.leetcode.tree.medium;
 import com.leetcode.tree.TreeNode;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class PathSumII_113_Impl implements PathSumII_113 {
 
   @Override
-  public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+  public List<List<Integer>> pathSum(final TreeNode root, final int targetSum) {
     final List<List<Integer>> result = new ArrayList<>();
-    final List<Integer> path = new ArrayList<>();
-    dfs(root, path, targetSum, result);
+    final Stack<Integer> path = new Stack<>();
+    fillPaths(root, path, targetSum, result);
     return result;
   }
 
-  private void dfs(
+  private void fillPaths(
+      // current context
       final TreeNode node,
-      final List<Integer> currentPath,
-      final int remainingSum,
+      final Stack<Integer> path,
+      final int targetSum,
+      // result
       final List<List<Integer>> result
   ) {
     if (node == null) {
       return;
     }
 
-    // add the current node val to path
-    currentPath.add(node.val);
-
-    if (node.left == null && node.right == null && remainingSum == node.val) {
-      result.add(new ArrayList<>(currentPath));
+    path.add(node.val);
+    if (isLeaf(node)) {
+      if (node.val == targetSum) {
+        result.add(new ArrayList<>(path));
+      }
     } else {
-      this.dfs(node.left, currentPath, remainingSum - node.val, result);
-      this.dfs(node.right, currentPath, remainingSum - node.val, result);
+      fillPaths(node.left, path, targetSum - node.val, result);
+      fillPaths(node.right, path, targetSum - node.val, result);
     }
 
-    // backtrack
-    currentPath.remove(currentPath.size() - 1);
+    // backtracking
+    path.pop();
+  }
+
+  private static boolean isLeaf(final TreeNode node) {
+    return node.left == null && node.right == null;
   }
 }
