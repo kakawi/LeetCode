200dcc337183508b9cb948f322c96ab422ea1dfa
Hleb Bandarenka <bhleb@amazon.com>
2023-04-24 19:15:25 -0700

    clarified 14

--- src/main/java/com/leetcode/string/easy/LongestCommonPrefix_14_HorizontalScanning.java
+++ src/main/java/com/leetcode/string/easy/LongestCommonPrefix_14_HorizontalScanning.java
@@ -7,11 +7,11 @@ public class LongestCommonPrefix_14_HorizontalScanning implements LongestCommonP
     String prefix = strs[0];
 
     for (int i = 1; i < strs.length; i++) {
-      while (!strs[i].startsWith(prefix)) {
-        prefix = prefix.substring(0 , prefix.length() - 1);
-        if (prefix.isEmpty())
-          return "";
+      final String currentString = strs[i];
+      while (!currentString.startsWith(prefix)) {
+        prefix = prefix.substring(0, prefix.length() - 1);
       }
+      if (prefix.isEmpty()) return "";
     }
 
     return prefix;
