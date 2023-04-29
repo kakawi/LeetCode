b7e841fa8bb3bd4268837e6f99a64ecf735f294c
Hleb Bandarenka <hleb.bandarenka@gmail.com>
2023-04-29 12:44:45 -0700

    improved 208

--- src/main/java/com/leetcode/design/medium/ImplementTrie_208_Impl.java
+++ src/main/java/com/leetcode/design/medium/ImplementTrie_208_Impl.java
@@ -1,5 +1,9 @@
 package com.leetcode.design.medium;
 
+import com.leetcode.tree.TreeNode;
+
+import java.util.Optional;
+
 public class ImplementTrie_208_Impl implements ImplementTrie_208 {
 
   private final TrieNode root = new TrieNode();
@@ -8,42 +12,45 @@ public class ImplementTrie_208_Impl implements ImplementTrie_208 {
   public void insert(final String word) {
     TrieNode currentNode = root;
     for (final char c : word.toCharArray()) {
-      if (currentNode.alphabet[c] == null) {
-        currentNode.alphabet[c] = new TrieNode();
+      final int index = getIndex(c);
+      if (currentNode.alphabet[index] == null) {
+        currentNode.alphabet[index] = new TrieNode();
       }
-      currentNode = currentNode.alphabet[c];
+      currentNode = currentNode.alphabet[index];
     }
     currentNode.isWord = true;
   }
 
   @Override
   public boolean search(final String word) {
-    TrieNode currentNode = root;
-    for (final char c : word.toCharArray()) {
-      if (currentNode.alphabet[c] != null) {
-        currentNode = currentNode.alphabet[c];
-      } else {
-        return false;
-      }
-    }
-    return currentNode.isWord;
+    return getTrieNode(word)
+            .map(trieNode -> trieNode.isWord)
+            .orElse(false);
   }
 
   @Override
   public boolean startsWith(final String prefix) {
+    return getTrieNode(prefix).isPresent();
+  }
+
+  private Optional<TrieNode> getTrieNode(String word) {
     TrieNode currentNode = root;
-    for (final char c : prefix.toCharArray()) {
-      if (currentNode.alphabet[c] != null) {
-        currentNode = currentNode.alphabet[c];
-      } else {
-        return false;
+    for (final char c : word.toCharArray()) {
+      final int index = getIndex(c);
+      if (currentNode.alphabet[index] == null) {
+        return Optional.empty();
       }
+      currentNode = currentNode.alphabet[index];
     }
-    return true;
+    return Optional.of(currentNode);
+  }
+
+  private static int getIndex(final char c) {
+    return c - 'a';
   }
 
   private static class TrieNode {
-    final TrieNode[] alphabet = new TrieNode[256];
+    final TrieNode[] alphabet = new TrieNode[26];
     boolean isWord;
   }
 }
