581d29865c966e7944388634b4cd4e3eb9f0e39b
Hleb Bandarenka <hleb.bandarenka@gmail.com>
2023-07-16 16:04:49 -0700

    improved tests 108

--- src/main/java/com/leetcode/binarysearchtree/TreeNode.java
+++ src/main/java/com/leetcode/binarysearchtree/TreeNode.java
@@ -1,5 +1,7 @@
 package com.leetcode.binarysearchtree;
 
+import java.util.Objects;
+
 public class TreeNode {
 
   public int val;
@@ -17,5 +19,18 @@ public class TreeNode {
     this.val = val;
     this.left = left;
     this.right = right;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    TreeNode node = (TreeNode) o;
+    return val == node.val && Objects.equals(left, node.left) && Objects.equals(right, node.right);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(val);
   }
 }
--- src/test/java/com/leetcode/binarysearchtree/easy/ConvertSortedArrayToBinarySearchTree_108Test.java
+++ src/test/java/com/leetcode/binarysearchtree/easy/ConvertSortedArrayToBinarySearchTree_108Test.java
@@ -1,6 +1,8 @@
 package com.leetcode.binarysearchtree.easy;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.AnyOf.anyOf;
+import static org.hamcrest.core.Is.is;
 
 import com.leetcode.binarysearchtree.TreeNode;
 import org.junit.jupiter.api.Test;
@@ -15,16 +17,20 @@ class ConvertSortedArrayToBinarySearchTree_108Test {
     final int[] nums = {-10, -3, 0, 5, 9};
 
     // expected
-    final TreeNode expected = new TreeNode(0,
-        new TreeNode(-3, new TreeNode(-10), null),
-        new TreeNode(9, new TreeNode(5), null)
+    final TreeNode expected1 = new TreeNode(0,
+            new TreeNode(-3, new TreeNode(-10), null),
+            new TreeNode(9, new TreeNode(5), null)
+    );
+    final TreeNode expected2 = new TreeNode(0,
+            new TreeNode(-10, null, new TreeNode(-3)),
+            new TreeNode(5, null, new TreeNode(9))
     );
 
     // when
     final TreeNode result = solution.sortedArrayToBST(nums);
 
     // then
-    assertEquals(expected, result);
+    assertThat(result, anyOf(is(expected1), is(expected2)));
   }
 
   @Test
@@ -33,12 +39,20 @@ class ConvertSortedArrayToBinarySearchTree_108Test {
     final int[] nums = {1, 3};
 
     // expected
-    final TreeNode expected = new TreeNode(3, new TreeNode(1), null);
+    final TreeNode expected1_1 = new TreeNode(3, new TreeNode(1), null);
+    final TreeNode expected1_2 = new TreeNode(3, null, new TreeNode(1));
+    final TreeNode expected2_1 = new TreeNode(1, new TreeNode(3), null);
+    final TreeNode expected2_2 = new TreeNode(1, null, new TreeNode(3));
 
     // when
     final TreeNode result = solution.sortedArrayToBST(nums);
 
     // then
-    assertEquals(expected, result);
+    assertThat(result, anyOf(
+            is(expected1_1),
+            is(expected1_2),
+            is(expected2_1),
+            is(expected2_2)
+    ));
   }
 }
