b89a255e20585a29ecd277b5bdfa0964ccfdb8c5
Hleb Bandarenka <hleb.bandarenka@intelex.com>
2024-12-16 12:40:04 -0800

    Simplified 973

--- src/main/java/com/leetcode/heap/medium/KClosestPointsToOrigin_973_Impl.java
+++ src/main/java/com/leetcode/heap/medium/KClosestPointsToOrigin_973_Impl.java
@@ -1,5 +1,6 @@
 package com.leetcode.heap.medium;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -12,14 +13,12 @@ import java.util.Queue;
 public class KClosestPointsToOrigin_973_Impl implements KClosestPointsToOrigin_973 {
 
   public int[][] kClosest(int[][] points, int k) {
-    Comparator<int[]> comparator = Comparator.comparingInt((point) -> {
-      return point[0] * point[0] + point[1] * point[1];
-    });
+    Comparator<int[]> comparator = Comparator.comparingInt(
+            (point) -> point[0] * point[0] + point[1] * point[1]
+    );
 
     Queue<int[]> queue = new PriorityQueue<>(comparator);
-    for (int[] point : points) {
-      queue.add(point);
-    }
+    Collections.addAll(queue, points);
 
     int[][] result = new int[k][2];
     for (int i = 0; i < k; i++) {
